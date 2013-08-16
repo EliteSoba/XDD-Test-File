ea38f7352f183e45bc3be568bbade3745d1f4cc2
Tobias Lee <tobiasle@usc.edu>
2013-08-16 09:08:09 -0700

    Create ArgumentPanel.java

---
+++ ArgumentPanel.java
@@ -0,0 +1,10 @@
+package xdd;
+
+import javax.swing.JPanel;
+
+public abstract class ArgumentPanel extends JPanel {
+	private static final long serialVersionUID = -4327817137461671209L;
+
+	public abstract String getArgs();
+	
+}
